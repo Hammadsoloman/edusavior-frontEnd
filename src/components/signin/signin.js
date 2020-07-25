52b104f0553f40c21d7637d290b7b3fbbdbf69e0
osamamousa204 <osamamousa204@gmail.com>
2020-07-25 19:01:36 +0300

    added sign in and signed up feature

---
+++ src/components/signin/signin.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState , useEffect} from 'react';
+
+// import Show from '../show/';
+
+import { Link, NavLink } from 'react-router-dom';
+
+const Login = (props) => {
+
+ const [user , setuser] =  useState({});
+
+ const handleChange = (e) => {
+  setuser({...user, [e.target.name]: e.target.value });
+  };
+
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    props.login(user.username, user.password);
+  };
+  
+
+    return (
+      <>
+          <span>Sign in</span>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter Username"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="password"
+              placeholder="Enter Password"
+              onChange={handleChange}
+            />
+            <button>Login</button>
+          </form>
+        
+      </>
+    );
+  
+}
+
+export default Login;
---
+++ src/hooks/auth/signup.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useState , useEffect} from 'react';
+import cookie from 'react-cookies';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import axios from 'axios';
+dotenv.config();
+const API = process.env.API_SERVER || 'https://edusavior-backend.herokuapp.com';
+const SECRET = process.env.JWT_SECRET || 'mysecret';
+
+//////////////======================(useSignup hook)=====================\\\\\\\\\\\\\\
+
+const  useSignup  = () => {
+    const [signedUp, setsignedUp] = useState(false);
+    const [userSign , setUser] =  useState({});
+    const [token , setToken] =  useState({});
+
+////////////////////////////////
+
+    const signup = (user) => {
+        console.log('ffffffff' , user );
+        axios({
+            url:`${API}/signup`,
+            method: 'post',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: { 'Content-Type': 'application/json' },
+            data: JSON.stringify(user)
+          })
+            .then(response => {
+        console.log('dddddddddddddd' , response.data.token );
+
+                validateToken(response.data.token);
+    
+                })
+            .catch(console.error);
+    }
+
+////////////////////////////////////
+
+    const logout = () => {
+        setSignupState(false, null, {});
+      }; 
+
+////////////////////////////////////  
+
+    const  setSignupState = (signedUp, token, user) => {
+        cookie.save('auth', token);
+        setsignedUp(signedUp);
+        setToken(token);
+        setUser(user);
+        // this.setState({ token, loggedIn, user });
+      };
+
+//////////////////////////////////
+
+    const  validateToken = (token) => {
+    try {
+
+      const user = jwt.verify(token, SECRET);
+      setSignupState(true, token, user);
+    } catch (e) {
+        setSignupState(false, null, {});
+      console.log('Token validation Error', e.message);
+    }
+  };
+
+return [signedUp,userSign,signup , logout]
+}
+
+export default useSignup;
