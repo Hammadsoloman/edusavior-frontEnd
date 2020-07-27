877b46505fe18bcc1eaeaf6e73746bb7ad64056e
your username <amalalmomani19@gmail.com>
2020-07-27 23:21:50 +0300

    not done

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,46 +1,61 @@
 import React from 'react';
-import { useState , useEffect} from 'react';
-
-import { Link, NavLink,withRouter } from 'react-router-dom';
-
+import { useState, useEffect } from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom';
+import '../signin/signin.scss'
 const Login = (props) => {
 
- const [user , setuser] =  useState({});
+  const [user, setuser] = useState({});
 
- const handleChange = (e) => {
-  setuser({...user, [e.target.name]: e.target.value });
+  const handleChange = (e) => {
+    setuser({ ...user, [e.target.name]: e.target.value });
   };
 
- const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     props.login(user.username, user.password);
     props.history.push('/')
   };
-  
 
-    return (
-      <>
-          <span>Sign in</span>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="username"
-              placeholder="Enter Username"
-              onChange={handleChange}
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter Password"
-              onChange={handleChange}
-            />
-            <button>Login</button>
-          </form>
-          <span>don't have an account! <Link to="/signup">signup</Link></span>
-        
-      </>
-    );
-  
+
+  return (
+    <>
+
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <div className="signin"><span >Sign in</span></div>
+            
+            <form onSubmit={handleSubmit}>
+              
+                <input
+                type="text" name="name" class="question" id="nme" required autocomplete="off" 
+                // type="text"
+                // name="username"
+                // placeholder="Enter Username"
+                // required
+                onChange={handleChange}
+              />
+              <label for="nme"><span>Username</span></label>
+              
+              <input
+            name="message" class="question" id="msg" required autocomplete="off"
+                // type="password"
+                // name="password"
+                // placeholder="Enter Password"
+                // required
+                onChange={handleChange}
+              />
+              <label for="msg"><span>Password</span></label>
+              <button>Login</button>
+            </form>
+            <span>don't have an account! <Link to="/signup">signup</Link></span>
+          </div>
+        </nav>
+      </div>
+
+    </>
+  );
+
 }
 
 export default withRouter(Login);
