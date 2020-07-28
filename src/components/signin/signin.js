71ed079691d51dfe0e4e36ca79fa680b06d42f9e
your username <amalalmomani19@gmail.com>
2020-07-28 21:51:54 +0300

    need header&footer

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -23,35 +23,53 @@ const Login = (props) => {
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
-            <div className="signin"><span >Sign in</span></div>
+            <div className="signin"><span >Log-In</span><p>Please login with your username and password!</p>
+            </div>
+            
+            {/* <form onSubmit={handleSubmit}>
+              <label>Username</label>
+                <input
+                type="text"
+                name="username"
+                placeholder="Enter Username"
+                onChange={handleChange}
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter Password"
+                onChange={handleChange}
+              />
+              <button>Login</button>
+            </form> */}
             
             <form onSubmit={handleSubmit}>
-              
+             
                 <input
-                type="text" name="name" class="question" id="nme" required autocomplete="off" 
-                // type="text"
-                // name="username"
-                // placeholder="Enter Username"
-                // required
-                onChange={handleChange}
-              />
-              <label for="nme"><span>Username</span></label>
+                type="text" name="username" className="sgininForm" id="nme" required 
+                onChange={handleChange} autocomplete="off"
+              /> <label for="nme"><span>Username</span></label>
               
               <input
-            name="message" class="question" id="msg" required autocomplete="off"
-                // type="password"
-                // name="password"
-                // placeholder="Enter Password"
-                // required
-                onChange={handleChange}
+               name="password" className="sgininForm" id="msg" required 
+               type="password"
+                onChange={handleChange} autocomplete="off"
               />
               <label for="msg"><span>Password</span></label>
-              <button>Login</button>
-            </form>
-            <span>don't have an account! <Link to="/signup">signup</Link></span>
+            <button className="button button1">Login</button>
+            <button className="button button2"><Link className="button3" to="/signup" >Signup</Link></button>
+            </form> 
+            <h1>OR</h1>
+            <button className="button7 button4"><Link className="button6" to="#" >Google</Link></button>
+            <button className="button7 button5"><Link  className="button6" to="#" >LinkedIn</Link></button>
+           {/* <img src="https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Girl in a jacket" width="500" height="600" fixed></img> */}
+            {/* <span>Don't have an account?</span> */}
           </div>
         </nav>
       </div>
+      <img src="https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Girl in a jacket" ></img>
+      <p className="paragraph">Welcome to the Edusavior</p>
 
     </>
   );
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,59 +1,71 @@
 import React from 'react';
-import { useState , useEffect} from 'react';
-
-// import Show from '../show/';
-
-import { Link, NavLink,withRouter } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import '../signup/signup.scss'
+import { Link, NavLink, withRouter } from 'react-router-dom';
 import useSignup from '../../hooks/auth/signup';
 
 const Signup = (props) => {
 
- const [user , setuser] =  useState({});
- const [signup] = useSignup();
- const handleChange = (e) => {
-  setuser({...user, [e.target.name]: e.target.value });
+  const [user, setuser] = useState({});
+  const [signup] = useSignup();
+  const handleChange = (e) => {
+    setuser({ ...user, [e.target.name]: e.target.value });
   };
 
- const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     e.target.reset();
     signup(user);
     props.history.push('/')
   };
-    return (
-      <>
-          <span>Sign up</span>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="username"
-              placeholder="Enter Username"
-              onChange={handleChange}
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter Password"
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter your email"
-              onChange={handleChange}
-            />
-            <select name="role" onChange={handleChange}>
-            <option selected disabled>Choose your role</option>
-                <option value='student'>student</option>
-                <option value='instructor'>instructor</option>
-            </select>
-            <button>Signup</button>
-          </form>
-          <span>back to <Link to="/">signin</Link></span>
-        
-      </>
-    );
-  
+  return (
+    <>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <div className="signin"><span>Sign up</span></div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="name"
+                className="sgininForm" id="nme"
+                autocomplete="off" required
+                onChange={handleChange}
+              />
+              <label for="nme"><span>Username</span></label>
+              <input
+                name="password" className="sgininForm" id="msg" required
+                type="password" autocomplete="off"
+                onChange={handleChange}
+              />
+              <label for="msg"><span>Password</span></label>
+              <input
+                name="email" className="sgininForm" id="mail" required
+                type="text" autocomplete="off"
+                onChange={handleChange}
+              />
+              <label for="mail"><span>Email</span></label>
+              <div className="select-wrapper">
+                <select className="select" name="role" onChange={handleChange}>
+                  <option selected disabled>Choose your role</option>
+                  <option value='student'>student</option>
+                  <option value='instructor'>instructor</option>
+                </select>
+              </div>
+              <button className="button button0">Signup</button>
+              <button className="button button9"><Link className="button03" to="/">Signin</Link></button>
+            </form>
+            
+
+          </div>
+        </nav>
+
+      </div>
+      <img className="img1" src="https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Girl in a jacket" ></img>
+      <p className="paragraph">Welcome to the Edusavior</p>
+    </>
+  );
+
 }
 
 export default withRouter(Signup);
